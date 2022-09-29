092bdcacbf15aa5fa0a080bc21afb1997a7c464e
ohah <bookyoon173@gmail.com>
2022-09-30 00:54:52 +0900

    검색기능 추가

--- src/pages/blog/list.rs
+++ src/pages/blog/list.rs
@@ -1,9 +1,15 @@
 use gloo_net::http::Request;
 use serde::{Deserialize, Serialize};
 use yew::{ function_component, html, use_effect_with_deps, use_state, Properties };
-use crate::{components::{card::Card, list_card::{Datas, ListCard}}, store::{toast::{ToastStatus}, blog::Blog}};
+use yew_router::{history, prelude::{use_history, use_location, Location}};
+use crate::{components::{card::Card, list_card::{Datas, ListCard}}, store::{toast::{ToastStatus}, blog::{Blog}}};
 use crate::components::pagination::Pagination;
 
+
+#[derive(Serialize, Deserialize, Clone, Properties, PartialEq, Default, Debug)]
+pub struct SearchQuery {
+  pub query:String,
+}
 
 #[derive(Default, Deserialize, Serialize, Debug, Clone)]
 pub struct ListResponse {
@@ -24,32 +30,71 @@ pub struct ListProps {
 #[function_component(List)]
 pub fn list(ListProps{ page }:&ListProps) -> Html {
   let lists = use_state(||ListResponse::default());
+  let history = use_history().unwrap();
+  let location = use_location().unwrap();
+  let query_string = {
+    let string = location.query::<SearchQuery>();
+    match string {
+      Ok(string) => {
+        string.query
+      },
+      Err(err) => {
+        String::from("")
+      }
+    }
+  };
   {
     let lists = lists.clone();
     let url = format!("/api/list/{}", page.clone());
-    use_effect_with_deps(move |page| {
-      wasm_bindgen_futures::spawn_local(async move {        
-        let fetched_list  = Request::get(url.as_str())
-          .header("accept", "application/json")
+    use_effect_with_deps(move |(page, query_string)| {
+      let page = page.clone();
+      let query_string = query_string.clone();
+      log::info!("{:?}", query_string);
+      if !query_string.is_empty() {
+        wasm_bindgen_futures::spawn_local(async move {
+          let query = format!("/api/search/?query={}&page={}",query_string, page);
+          let fetched_list = Request::get(query.as_str())
+          .header("accept", "applcation/json")
           .header("Access-Control-Allow-Origin", "no-cors")
           .send()
           .await
           .unwrap()
           .json::<ListResponse>()
           .await;
-        match fetched_list {
-          Ok(fetched_list) => {
-            lists.set(fetched_list);
-          }
-          Err(err) => {
-            log::info!("Update: {:?}", err);
-            Blog::toast_message("알 수 없는 오류가 발생헀습니다.", ToastStatus::DANGER, None);
-          }
-        };
-        log::info!("Update: {:?}", lists);
-      });
+          match fetched_list {
+            Ok(fetched_list) => {
+              lists.set(fetched_list);
+            }
+            Err(err) => {
+              log::info!("Update: {:?}", err);
+              Blog::toast_message("알 수 없는 오류가 발생헀습니다.", ToastStatus::DANGER, None);
+            }
+          };
+        });
+      } else {
+        wasm_bindgen_futures::spawn_local(async move {        
+          let fetched_list  = Request::get(url.as_str())
+            .header("accept", "application/json")
+            .header("Access-Control-Allow-Origin", "no-cors")
+            .send()
+            .await
+            .unwrap()
+            .json::<ListResponse>()
+            .await;
+          match fetched_list {
+            Ok(fetched_list) => {
+              lists.set(fetched_list);
+            }
+            Err(err) => {
+              log::info!("Update: {:?}", err);
+              Blog::toast_message("알 수 없는 오류가 발생헀습니다.", ToastStatus::DANGER, None);
+            }
+          };
+          log::info!("Update: {:?}", lists);
+        });
+      }
       || ()
-    }, page.clone());
+    }, (page.clone(), query_string.clone()));
   }
   
 	html! {
